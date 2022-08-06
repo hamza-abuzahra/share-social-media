93ad2400d353629473ae9b6b8bdebea94b66b8c6
hamza abuzahra <hka201210@hotmail.com>
2022-08-06 17:05:17 +0300

    8th attempt

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -10,9 +10,33 @@ export const useCounterStore = defineStore({
   },
   actions: {
     async share() {
-      const res = await fetch("src\\assets\\WhatsApp Image 2022-07-27 at 2.52.30 PM.jpeg")
-      const blob = await res.blob()
-      const files = [new File([blob], 'img.jpeg', {type: blob.type })]
+      let files = []
+      let url = 'src\\assets\\WhatsApp Image 2022-07-27 at 2.52.30 PM.jpeg'
+      const toDataURL = async (url) => fetch(url)
+      .then(response => response.blob())
+      .then(blob => new Promise((resolve, reject) => {
+      const reader = new FileReader()
+      reader.onloadend = () => resolve(reader.result)
+      reader.onerror = reject
+      reader.readAsDataURL(blob)
+     }))
+     function dataURLtoFile(dataurl, filename) {
+      var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+      bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+      while(n--){
+      u8arr[n] = bstr.charCodeAt(n);
+      }
+        return new File([u8arr], filename, {type:mime});
+      }
+
+
+      await toDataURL(url)
+      .then(dataUrl => {
+        console.log('Here is Base64 Url', dataUrl)
+        var fileData = dataURLtoFile(dataUrl, "imageName.jpg");
+        console.log("Here is JavaScript File Object",fileData)
+        files.push(fileData)
+      })
 
       console.log(files)
       if (files.length === 0) {
@@ -22,8 +46,8 @@ export const useCounterStore = defineStore({
       try {
         await navigator.share({
           files,
-          // title: 'Images',
-          // text: 'Beautiful images'
+          title: 'Images',
+          text: 'Beautiful images'
         })
         console.log('Shared!')
       } catch (error) {
