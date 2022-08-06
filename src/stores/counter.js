e4444a6465997ef0084732035e483ac8c9eeae6e
hamza abuzahra <hka201210@hotmail.com>
2022-08-06 17:37:11 +0300

    10th attempt

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -10,21 +10,23 @@ export const useCounterStore = defineStore({
   },
   actions: {
     async share() {
-
-      const files = document.getElementById('test').files
-      if (files.length === 0) {
-        console.log('No files selected.')
-        return
-      }
+      let shareimg = "src\\assets\\WhatsApp Image 2022-07-27 at 2.52.30 PM.jpeg"
+      let sharetitle = "test"
+      let sharetext = "please work"
+      let shareurl = "whatever.com"
       try {
+        const response = await fetch(shareimg);
+        const blob = await response.blob();
+        const file = new File([blob], 'rick.jpg', {type: blob.type});
+  
         await navigator.share({
-          files,
-          title: 'Images',
-          text: 'Beautiful images'
-        })
-        console.log('Shared!')
-      } catch (error) {
-        console.log(`Error: ${error.message}`)
+          url: shareurl,
+          title: sharetitle,
+          text: sharetext,
+          files: [file]
+        });
+      } catch (err) {
+        console.log(err.name, err.message);
       }
     },
   } 
