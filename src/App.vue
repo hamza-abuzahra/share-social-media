c66eeb468308c77a57cf5000f27f932c3f14888d
hamza abuzahra <hka201210@hotmail.com>
2022-08-06 19:01:09 +0300

    15th attempt

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@ const share = async function() {
 
 <template>
   <header>
-    <img alt="Vue logo" id="shared" class="logo" src="@\assets\test.jpeg" width="125" height="125" />
+    <img alt="Vue logo" id="shared" class="logo" src="@\assets\test3.jpeg"/>
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
@@ -41,67 +41,3 @@ const share = async function() {
 
   <RouterView />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
