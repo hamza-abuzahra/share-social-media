83792f2b4b55e87bceb69bce3da934f0a944bdf5
hamza abuzahra <hka201210@hotmail.com>
2022-08-06 16:15:25 +0300

    3rd attempt

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -12,21 +12,27 @@ export const useCounterStore = defineStore({
     share() {
       console.log('activated')
       const urlToObject = async()=> {
-        const response = await fetch("src\\assets\\logo.svg");
+        const response = await fetch(`src\\assets\\WhatsApp Image 2022-07-27 at 2.52.30 PM.jpeg`);
         // here image is url/location of image
         const blob = await response.blob();
         const file = [new File([blob], 'image.jpg', {type: blob.type})];
+        // console.log(file)
         return file
       }
-      let file = urlToObject()
-
-      const shareData = {
+      let file = urlToObject().then((file) => {
+        console.log(file)
+        const shareData = {
           text: "test", 
           title: "please work", 
           file
-      }
-      navigator.share(shareData)
-      console.log("are you here")
+        }
+        navigator.share(shareData)
+        console.log("are you here")
+      })
+
+      // console.log(file)
+      
+
     }
   }
 })
