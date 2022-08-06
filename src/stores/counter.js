61b4d5caf4a8bffc5621de4a3aca4f9f340765ff
hamza abuzahra <hka201210@hotmail.com>
2022-08-06 16:44:02 +0300

    6th attempt

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -3,41 +3,44 @@ import { defineStore } from 'pinia'
 export const useCounterStore = defineStore({
   id: 'counter',
   state: () => ({
-    counter: 0
+    counter: 0,
   }),
   getters: {
     doubleCount: (state) => state.counter * 2
   },
   actions: {
-    share() {
-      ;(async () => {
-        if (!('share' in navigator)) {
-          return
-        }
-        // `element` is the HTML element you want to share.
-        // `backgroundColor` is the desired background color.
-        fetch("src\\assets\\WhatsApp Image 2022-07-27 at 2.52.30 PM.jpeg").then(res => res.blob()).then(async (blob) => {
-            // Even if you want to share just one file you need to
-            // send them as an array of files.
-            const files = [new File([blob], 'image.jpeg', { type: blob.type })]
-            const shareData = {
-              text: 'Some text',
-              title: 'Some title',
-              files,
-            }
-            if (navigator.canShare(shareData)) {
-              try {
-                await navigator.share(shareData)
-              } catch (err) {
-                if (err.name !== 'AbortError') {
-                  console.error(err.name, err.message)
-                }
-              }
-            } else {
-              console.warn('Sharing not supported', shareData)
-            }
+    async share() {
+      const res = await fetch("src\\assets\\WhatsApp Image 2022-07-27 at 2.52.30 PM.jpeg")
+      const blob = await res.blob()
+      const files = [new File([blob], 'img.jpeg', {type: blob.type })]
+
+      console.log(files)
+      if (files.length === 0) {
+        console.log('No files selected.')
+        return
+      }
+    
+      // feature detecting navigator.canShare() also implies
+      // the same for the navigator.share()
+      // if (!navigator.canShare) {
+      //   output.textContent = `Your browser doesn't support the Web Share API.`
+      //   return
+      // }
+    
+      // if (navigator.canShare({ files })) {
+        try {
+          await navigator.share({
+            files,
+            title: 'Images',
+            text: 'Beautiful images'
           })
-    })()
-  },
-  }
+          console.log('Shared!')
+        } catch (error) {
+          console.log(`Error: ${error.message}`)
+        }
+      // } else {
+      //   output.textContent = `Your system doesn't support sharing these files.`
+      // }
+    },
+  } 
 })
