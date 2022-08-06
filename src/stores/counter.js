5c1545cb28127ac5833c3ff7314be953e36953b6
hamza abuzahra <hka201210@hotmail.com>
2022-08-06 18:19:32 +0300

    12th attempt

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -10,21 +10,27 @@ export const useCounterStore = defineStore({
   },
   actions: {
     async share() {
-      let shareimg = "src\\assets\\WhatsApp Image 2022-07-27 at 2.52.30 PM.jpeg"
-      let sharetitle = "test"
-      let sharetext = "please work"
-      let shareurl = "whatever.com"
-      try {
-        const response = await fetch(shareimg);
-        const blob = await response.blob();
-        const file = new File([blob], 'rick.jpg', {type: blob.type});
-  
-        await navigator.share({
-          files: [file]
-        });
-      } catch (err) {
-        console.log(err.name, err.message);
-      }
+      fetch("src\\assets\\WhatsApp Image 2022-07-27 at 2.52.30 PM.jpeg")
+      .then(function(response) {
+        return response.blob()
+      })
+      .then(function(blob) {
+
+        var file = new File([blob], "picture.jpg", {type: 'image/jpeg'});
+        var filesArray = [file];
+
+        try {
+          navigator.share({
+            text: 'some_text',
+            files: filesArray,
+            title: 'some_title',
+            url: 'some_url'
+          });
+        } catch (err){
+          console.log(err)
+        }
+      })
+
     },
   } 
 })
